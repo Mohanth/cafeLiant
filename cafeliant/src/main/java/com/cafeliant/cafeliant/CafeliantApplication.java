58aed779999e0fcb26d0ead9ebb185b4eed03a05
mohanth <mohanthvanaparthi6@gmail.com>
2020-04-25 12:46:13 +0530

    Initial setup

---
+++ cafeliant/src/main/java/com/cafeliant/cafeliant/CafeliantApplication.java
@@ -0,0 +1,13 @@
+package com.cafeliant.cafeliant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeliantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeliantApplication.class, args);
+	}
+
+}
---
+++ cafeliant/src/test/java/com/cafeliant/cafeliant/CafeliantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeliant.cafeliant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeliantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
